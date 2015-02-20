b65ec5762c870546dd029f1d9ed1041e77a66dc4
daniel.nee <daniel.nee@buchanan-edwards.com>
2015-02-20 11:03:25 -0500

    Added FunWithDevs Project

---
+++ FunWithDevs/FunWithDevs/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FunWithDevs.Startup))]
+namespace FunWithDevs
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
